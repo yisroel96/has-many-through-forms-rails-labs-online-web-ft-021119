43359f7a2d89fdb9bb4ea4dbe8f203e2255b2382
github username <github email address>
2019-05-07 22:07:02 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,13 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
   def show
     @category = Category.find(params[:id])
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
-  has_many :post_categories
-  has_many :categories, through: :post_categories
-  has_many :comments
-  has_many :users, through: :comments
+has_many :post_categories
+has_many :categories, through: :post_categories
+has_many :comments
+has_many :users, through: :comments
 
-
+accepts_nested_attributes_for :categories
 end
